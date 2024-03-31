f5951a1907bb02d19d427c26d39ef69a82fe84bc
MaSungHo <tonem123@naver.com>
2024-03-31 20:22:34 +0900

    feat: initialize project

---
+++ src/main/kotlin/com/lol/analyzer/aram/AramApplication.kt
@@ -0,0 +1,11 @@
+package com.lol.analyzer.aram
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AramApplication
+
+fun main(args: Array<String>) {
+	runApplication<AramApplication>(*args)
+}
---
+++ src/test/kotlin/com/lol/analyzer/aram/AramApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lol.analyzer.aram
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AramApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
