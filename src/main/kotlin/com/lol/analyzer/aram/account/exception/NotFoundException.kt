7078bb6deec325394bdb3386405638372ad7d66f
MaSungHo <tonem123@naver.com>
2024-04-16 21:25:44 +0900

    :hammer: account not found exception

---
+++ src/main/kotlin/com/lol/analyzer/aram/account/exception/NotFoundException.kt
@@ -0,0 +1,10 @@
+package com.lol.analyzer.aram.account.exception
+
+import com.lol.analyzer.aram.common.enums.ErrorCode
+
+
+class NotFoundException(
+    override val message: String,
+    override var errorCode: ErrorCode = ErrorCode.RECORD_NOT_FOUND
+): AccountException(message) {
+}
