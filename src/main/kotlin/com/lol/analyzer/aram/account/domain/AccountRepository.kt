315cf4afba16d800cbd23624231ab0ce81b8b7ef
MaSungHo <tonem123@naver.com>
2024-04-16 20:54:48 +0900

    :hammer: findByPuuid -> findByUuid

--- src/main/kotlin/com/lol/analyzer/aram/account/domain/AccountRepository.kt
+++ src/main/kotlin/com/lol/analyzer/aram/account/domain/AccountRepository.kt
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 interface AccountRepository: JpaRepository<Account, Long> {
-    fun findByPuuid(puuid: String): Account?
+    fun findByUuid(uuid: String): Account?
     fun findByGameNameAndTagLine(gameName: String, tagLine: String): Account?
 }
--- src/main/kotlin/com/lol/analyzer/aram/account/dto/AccountResponse.kt
+++ src/main/kotlin/com/lol/analyzer/aram/account/dto/AccountResponse.kt
@@ -3,12 +3,13 @@ package com.lol.analyzer.aram.account.dto
 import com.lol.analyzer.aram.account.domain.Account
 
 data class AccountResponse(
+        val uuid: String?,
         val gameName: String,
         val tagLine: String,
 ) {
     companion object {
         fun from(account: Account): AccountResponse {
-            return AccountResponse(account.gameName, account.tagLine)
+            return AccountResponse(account.uuid, account.gameName, account.tagLine)
         }
     }
 }
