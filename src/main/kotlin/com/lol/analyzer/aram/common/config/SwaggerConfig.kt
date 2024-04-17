5ee07098b0e0000afb7f56d05169fd4a451685b9
MaSungHo <tonem123@naver.com>
2024-04-17 21:04:13 +0900

    :hammer: divide swagger by service

--- src/main/kotlin/com/lol/analyzer/aram/common/config/SwaggerConfig.kt
+++ src/main/kotlin/com/lol/analyzer/aram/common/config/SwaggerConfig.kt
@@ -5,11 +5,39 @@ import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
 import io.swagger.v3.oas.models.security.SecurityRequirement
 import io.swagger.v3.oas.models.security.SecurityScheme
+import org.springdoc.core.models.GroupedOpenApi
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class SwaggerConfig {
+    @Bean
+    fun tftAPI(): GroupedOpenApi {
+        return GroupedOpenApi
+            .builder()
+            .group("TFT")
+            .pathsToMatch("/api/tft/**")
+            .build()
+    }
+
+    @Bean
+    fun accountAPI(): GroupedOpenApi {
+        return GroupedOpenApi
+            .builder()
+            .group("Account")
+            .pathsToMatch("/api/accounts/**")
+            .build()
+    }
+
+    @Bean
+    fun leagueOfLegendAPI(): GroupedOpenApi {
+        return GroupedOpenApi
+            .builder()
+            .group("League of Legend")
+            .pathsToMatch("/api/lol/**")
+            .build()
+    }
+
     @Bean
     fun openAPI(): OpenAPI {
         val jwt = "JWT"
