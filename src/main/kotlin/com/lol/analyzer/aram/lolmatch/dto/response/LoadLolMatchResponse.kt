2842e16c56c3451e0d2db39c0fefc1a956236c5a
MaSungHo <tonem123@naver.com>
2024-05-11 17:36:11 +0900

    :tada: long value to date format

--- src/main/kotlin/com/lol/analyzer/aram/lolmatch/dto/response/LoadLolMatchResponse.kt
+++ src/main/kotlin/com/lol/analyzer/aram/lolmatch/dto/response/LoadLolMatchResponse.kt
@@ -5,6 +5,8 @@ import com.lol.analyzer.aram.common.enums._enums.GameType
 import com.lol.analyzer.aram.common.enums._enums.Maps
 import com.lol.analyzer.aram.lolmatch.domain.LolMatch
 import io.swagger.v3.oas.annotations.media.Schema
+import java.text.SimpleDateFormat
+import java.util.*
 
 @Schema(name = "Load Lol Match", description = "LoL Match 불러오기")
 data class LoadLolMatchResponse(
@@ -17,11 +19,11 @@ data class LoadLolMatchResponse(
     @Schema(description = "게임 타입")
     val gameType: GameType,
     @Schema(description = "게임 시작시간")
-    val gameStartTimestamp: Long,
+    val gameStartTimestamp: String,
     @Schema(description = "게임 종료시간")
-    val gameEndTimestamp: Long,
+    val gameEndTimestamp: String,
     @Schema(description = "계정 정보")
-    val accountLolMatches: List<LoadAccountLolMatchResponse> = listOf()
+    val accounts: List<LoadAccountLolMatchResponse> = listOf()
 ) {
     companion object {
         fun from(lolMatch: LolMatch) = LoadLolMatchResponse(
@@ -29,9 +31,15 @@ data class LoadLolMatchResponse(
             mapId = lolMatch.mapId,
             gameMode = lolMatch.gameMode,
             gameType = lolMatch.gameType,
-            gameStartTimestamp = lolMatch.gameStartTimestamp,
-            gameEndTimestamp = lolMatch.gameEndTimestamp,
-            accountLolMatches = lolMatch.accountLolMatches.map { LoadAccountLolMatchResponse.from(it) }
+            gameStartTimestamp = toDate(lolMatch.gameStartTimestamp),
+            gameEndTimestamp = toDate(lolMatch.gameEndTimestamp),
+            accounts = lolMatch.accountLolMatches.map { LoadAccountLolMatchResponse.from(it) }
         )
+
+        private fun toDate(timestamp: Long): String {
+            val date = Date(timestamp)
+            val f = SimpleDateFormat("yyyy/MM/dd HH:mm", Locale.KOREA)
+            return f.format(date)
+        }
     }
 }
